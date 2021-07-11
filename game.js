f307693666b0bfbb30671b4c2d65924707d8b498
rimpal1801 <75491344+rimpal1801@users.noreply.github.com>
2021-07-11 07:38:21 -0700

    Update game.js

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ var userClickedPattern=[];
 var started = false;
 var level =0;
 
-$("#start").click(function () {
+$(".btn").click(function () {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
